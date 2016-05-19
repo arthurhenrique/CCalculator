561e55b00a13fb1a13f8f5ebdaa16bae66ccffdd
arthurhenrique <arthur393@gmail.com>
2016-05-18 21:56:19 -0300

    identation 1

--- CCalculator/Main.cpp
+++ CCalculator/Main.cpp
@@ -7,10 +7,12 @@ int main(int argc, char **argv)
 {
     CCalculator *calculator = new CCalculator();
 
-    cout << "Sum: "      << calculator->Sum(10, 10) << endl;
+    cout << "Sum: "      << calculator->Sum(10, 10)         << endl;
     cout << "Subtract: " << calculator->Subtract(0.1, 0.01) << endl;
-    cout << "Division: " << calculator->Division(2, 10) << endl;
-    cout << "Multiply: " << calculator->Multiply(3, 30) << endl;
+    cout << "Division: " << calculator->Division(20, 10)    << endl;
+    cout << "Multiply: " << calculator->Multiply(3, 30)     << endl;
+
+    delete calculator;
 
 
     int stop;
